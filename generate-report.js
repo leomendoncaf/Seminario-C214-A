bc304495cdf652f135cc4660b742a1ab829d92a7
Leo Franco <leonardo.franco@ges.inatel.br>
2023-06-02 13:14:43 -0300

    att

--- generate-report.js
+++ generate-report.js
@@ -10,10 +10,10 @@ runTests();
 
 async function runTests() {
   try {
-    const { results } = await run(['StackTest.test.js']); // Substitua o nome do arquivo de teste pelo nome correto
+    await run([]);
 
-    // Crie o conteúdo do relatório com base nos resultados dos testes
-    const reportContent = generateReportContent(results);
+    // Crie o conteúdo do relatório
+    const reportContent = 'Este é o conteúdo do relatório.';
 
     // Escreva o conteúdo do relatório no arquivo
     fs.writeFileSync(reportPath, reportContent);
@@ -23,31 +23,4 @@ async function runTests() {
   } catch (error) {
     console.error('Ocorreu um erro ao executar os testes:', error);
   }
-}
-
-function generateReportContent(results) {
-  // Implemente a lógica para gerar o conteúdo do relatório com base nos resultados dos testes
-  // Você pode percorrer os resultados e extrair informações relevantes, como os resultados dos testes, estatísticas etc.
-  // Por exemplo, você pode criar uma string com os resultados dos testes formatados.
-
-  let reportContent = '';
-
-  results.testResults.forEach((testResult) => {
-    reportContent += `Test Suite: ${testResult.testFilePath}\n`;
-    reportContent += `Total Tests: ${testResult.numTotalTests}\n`;
-    reportContent += `Passed Tests: ${testResult.numPassedTests}\n`;
-    reportContent += `Failed Tests: ${testResult.numFailedTests}\n`;
-    reportContent += '\n';
-
-    testResult.testResults.forEach((testCase) => {
-      reportContent += `Test Case: ${testCase.fullName}\n`;
-      reportContent += `Status: ${testCase.status}\n`;
-      reportContent += `Duration: ${testCase.duration}ms\n`;
-      reportContent += '\n';
-    });
-
-    reportContent += '\n';
-  });
-
-  return reportContent;
 }
