a1e25696527cffb548583d90808a0bef3cf00bba
Leo Franco <leonardo.franco@ges.inatel.br>
2023-06-02 13:20:11 -0300

    att

--- generate-report.js
+++ generate-report.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { run } = require('jest');
+const { runCLI } = require('jest');
 
 // Defina o caminho para o arquivo de relatório
 const reportPath = path.join(__dirname, 'report.txt');
@@ -10,10 +10,17 @@ runTests();
 
 async function runTests() {
   try {
-    await run([]);
+    const { results } = await runCLI(
+      {
+        config: path.join(__dirname, 'jest.config.js'),
+        silent: true,
+        watchman: false,
+      },
+      [__dirname]
+    );
 
-    // Crie o conteúdo do relatório
-    const reportContent = 'Este é o conteúdo do relatório.';
+    // Acesse os resultados dos testes para gerar o conteúdo do relatório
+    const reportContent = generateReportContent(results);
 
     // Escreva o conteúdo do relatório no arquivo
     fs.writeFileSync(reportPath, reportContent);
@@ -23,4 +30,23 @@ async function runTests() {
   } catch (error) {
     console.error('Ocorreu um erro ao executar os testes:', error);
   }
+}
+
+function generateReportContent(results) {
+  // Implemente a lógica para gerar o conteúdo do relatório com base nos resultados dos testes
+  // Você pode percorrer os resultados e extrair as informações relevantes para o relatório
+
+  // Exemplo: Gere um relatório simples com o número de testes executados
+  const totalTests = results.numTotalTests;
+  const passedTests = results.numPassedTests;
+  const failedTests = results.numFailedTests;
+
+  const reportContent = `
+    <h1>Relatório de Testes</h1>
+    <p>Total de testes: ${totalTests}</p>
+    <p>Testes passados: ${passedTests}</p>
+    <p>Testes falhados: ${failedTests}</p>
+  `;
+
+  return reportContent;
 }
